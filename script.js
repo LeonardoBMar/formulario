6f45f0631a409ac1094523f30a4452352aed6e48
unknown <leonardobozola@gmail.com>
2024-05-14 23:20:43 -0300

    formulario

---
+++ script.js
@@ -0,0 +1,102 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const form = document.getElementById('mainForm');
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const phone = document.getElementById('phone');
+    const password = document.getElementById('password');
+    const message = document.getElementById('message');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        if (checkInputs()) {
+            showModal();
+        }
+    });
+
+    name.addEventListener('input', () => {
+        validateField(name, name.value.trim() !== '', 'O nome não pode estar em branco');
+    });
+
+    email.addEventListener('input', () => {
+        validateField(email, isEmail(email.value.trim()), 'E-mail Invalido');
+    });
+
+    phone.addEventListener('input', () => {
+        validateField(phone, isPhone(phone.value.trim()), 'Telefone Invalido');
+    });
+
+    password.addEventListener('input', () => {
+        validateField(password, password.value.trim().length >= 8, 'A senha deve conter pelo menos 8 caracteres');
+    });
+
+    message.addEventListener('input', () => {
+        validateField(message, message.value.trim() !== '', 'A mensagem não pode estar em branco');
+    });
+
+    function checkInputs() {
+        let isValid = true;
+        validateField(name, name.value.trim() !== '', 'O nome não pode estar em branco');
+        validateField(email, isEmail(email.value.trim()), 'E-mail Invalido');
+        validateField(phone, isPhone(phone.value.trim()), 'Telefone Invalido');
+        validateField(password, password.value.trim().length >= 8, 'A senha deve conter pelo menos 8 caracteres');
+        validateField(message, message.value.trim() !== '', 'A mensagem não pode estar em branco');
+
+        document.querySelectorAll('.form-control').forEach((control) => {
+            if (control.classList.contains('error')) {
+                isValid = false;
+            }
+        });
+
+        return isValid;
+
+    }
+
+    function validateField(input, condition, errorMessage) {
+        if (condition) {
+            setSuccess(input);
+        } else {
+            setError(input, errorMessage);
+        }
+    }
+
+    function setError(input, message) {
+        const formControl = input.parentElement;
+        const icon = formControl.querySelector('.icon');
+        formControl.className = 'form-control error';
+        icon.className = 'icon fas fa-times-circle';
+        input.placeholder = message;
+    }
+
+    function setSuccess(input) {
+        const formControl = input.parentElement;
+        const icon = formControl.querySelector('.icon');
+        formControl.className = 'form-control success';
+        icon.className = 'icon fas fa-check-circle';
+    }
+
+    function isEmail(email) {
+        return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email);
+    }
+
+    function isPhone(phone) {
+        return /^\+?(\d.*){3,}$/.test(phone);
+    }
+
+    function showModal() {
+        const modal = document.getElementById('successModal');
+        modal.style.display = 'block';
+
+        const closeBtn = document.querySelector('.close-button');
+        closeBtn.onclick = function () {
+            modal.style.display = 'none';
+        };
+
+        window.onclick = function (event) {
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        };
+    }
+
+});
